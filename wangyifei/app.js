10a41ac88baf98f0ef4ebfd0afa23baa61f93741
wangyife <1152993632@qq.com>
2016-08-28 16:43:41 +0800

    es655

--- wangyifei/app.js
+++ wangyifei/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 
+
 var path = require('path');//处理文件路径的 join resolve
 var favicon = require('serve-favicon');//处理收藏夹图标
 var logger = require('morgan');//是一个请求日志打印工具
@@ -26,6 +27,9 @@ app.engine('html',require('ejs').__express);//设置渲染函数
 // 在你将收藏夹图标放置在/public目录下之后就可以取消掉此注释了
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));//使用请求日志中间件
+var fs=require('fs');
+//var accessStream=fs.createWriteStream('../access.log');
+//app.use(logger('dev',{stream:accessStream}));
 //通过判断请求头中的content-type来得到请求体的内容类型
 //如果是content-type=application/json req.body=JSON.parse(请求体)
 app.use(bodyParser.json());//处理json格式请求体 {name:'zfpx'}
@@ -56,6 +60,8 @@ app.use(function(req,res,next){
     //把session中的user属性赋给模板数据对象的user属性
     //如果已登陆，则 req.session.user有值，如果未登陆，则 req.session.user没有值
     res.locals.user = req.session.user;
+    //给关键字默认值
+    res.locals.keyword = '';
     next();
 });
 //静态文件中间件 根目录是public目录,所以在页面中引用静态文件的时候必须以public目录作为根目录
@@ -68,18 +74,22 @@ app.use('/user', user);
 app.use('/article', article);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    console.log(req.url);
+    //var err = new Error('Not Found');
+    //err.status = 404;
+
+    //next(err);
+    res.render('404');
 });
 
 // error handlers
-
+//var errorStream=fs.createWriteStream('./error.log');
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         console.error(err);//把错误输出到控制台
+        errorStream.write(err.toString());
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
--- wangyifei/routes/article.js
+++ wangyifei/routes/article.js
@@ -4,46 +4,94 @@ var markdown = require('markdown').markdown;
 //创建一个路由容器
 var router = express.Router();
 //文章列表路由
-router.get('/list',auth.mustLogin,function(req,res){
+router.get('/list', auth.mustLogin, function (req, res) {
     var user = req.query.user;//取得查询字符串中的用户ID
+    var keyword = req.query.keyword;//取得查询字关键字
     //读取所有的列表并显示在页面中
     var query = {};//查询条件对象
-    if(user)
+    /**
+     * 要实现分页 需要让客户端知道 当前是第几页，每页多少条,一共多少页
+     *
+     */
+    var pageNum = parseInt(req.query.pageNum || 1); //当前的页码
+    var pageSize = parseInt(req.query.pageSize || 3);//每页的条数
+    var order = req.query.order;
+    //如果有用户ID的话查询此用户的所有的文章
+    if (user)
         query['user'] = user;
-    Model('Article').find(query).populate('user').exec(function(err,docs){
-        if(err){
-            req.flash('error','显示文章列表失败'+err);
-            res.redirect('back');
-        }else{
+    //如果用户输入的关键字的话，那么查询标题和正文中包含此关键字的文章
+    if (keyword) {
+        var filter = new RegExp(keyword);//先写正则
+        //或条件，如果title符合正则或者 内容符合正则
+        query["$or"] = [{title: filter}, {content: filter}];
+    }
+    //默认情况下按文章的发表顺序倒序排列
+    var defaultOrder = {createAt: -1};
+    if (order) { // createAt -createAt title -title
+        var orderValue = 1;//默认排序顺序 1
+        if (order.startsWith('-')) {//表示倒序排列
+            orderValue = -1;//表示要倒序
+            order = order.slice(1);//去掉-之后就成为真正排序字段名称了
+        }
+        defaultOrder[order] = orderValue;
+    }
+    var count;
+    Model('Article')
+        .count(query)
+        .then(function (result) {//得到符合这个条件的总条数
+            count = result;
+            return Model('Article')
+                .find(query)//按指定的条件过滤
+                .sort(defaultOrder)
+                .skip((pageNum - 1) * pageSize) //跳过指定的条数
+                .limit(pageSize)//限定返回的条数
+                .populate('user')//把user属性从ID转成对象
+                .exec();//开始真正执行查询，返回一个新promise
+        }).then(function (docs) {//docs是当前页的文章列表
             //把markdown源文件转换成html格式的内容
-            docs.forEach(function(doc){
+            docs.forEach(function (doc) {
                 doc.content = markdown.toHTML(doc.content);
             });
             //docs是所有的文章数组
-            res.render('article/list',{title:'文章列表',articles:docs});
-        }
+            res.render('article/list', {
+                title: '文章列表',
+                articles: docs,//当前页的文章列表
+                keyword: keyword,//关键字
+                pageNum: pageNum,//当前页
+                pageSize: pageSize,//每页多少条
+                order:order,
+                totalPages: Math.ceil(count / pageSize) //总页数
+            });
+        }).catch(function (err) {
+            req.flash('error', '显示文章列表失败' + err);
+            res.redirect('back');
+        });
 
-    });
 });
 
 //显示增加文章的表单路由
-router.get('/add',auth.mustLogin,function(req,res){
+router.get('/add', auth.mustLogin, function (req, res) {
     //新增加文章的时候article是空对象
-    res.render('article/add',{title:'新增文章',article:{}});
+    res.render('article/add', {title: '新增文章', article: {}});
 });
 
-router.post('/add',auth.mustLogin,function(req,res){
+router.post('/add', auth.mustLogin, function (req, res) {
     //从请求体中得到文档对象
     var article = req.body;
     var articleId = article._id;
-    if(articleId){//如果ID有值表示修改文章
-        Model('Article').update({_id:articleId},{$set:{
-            title:article.title,//能且只能更新title和content两个字段
-            content:article.content//指定修改后的内容
-        }}).then(function(result){
-            res.redirect('/article/detail/'+articleId);
+    if (articleId) {//如果ID有值表示修改文章
+        Model('Article').update({_id: articleId}, {
+            $set: {
+                title: article.title,//能且只能更新title和content两个字段
+                content: article.content//指定修改后的内容
+            }
+        }).then(function (result) {
+            res.redirect('/article/detail/' + articleId);
+        }, function (error) {
+            req.flash('error', error);
+            res.redirect('back');
         });
-    }else{//如果ID没有值表示新增加文章
+    } else {//如果ID没有值表示新增加文章
         //从会话对象session中取出userId,赋给文章的发表人字段
         article.user = req.session.user._id;
         //因为新增的时候_id传过来的是空字符串，因为_id主键不能为空,所以要删除此字段
@@ -51,12 +99,12 @@ router.post('/add',auth.mustLogin,function(req,res){
         //如果_id为null,mongodb会帮我们自动生成一个合法_id值
         delete article._id;
         //把此文件存放到数据库里
-        Model('Article').create(article,function(err,doc){
-            if(err){
-                req.flash('error','文章发表失败了');
+        Model('Article').create(article, function (err, doc) {
+            if (err) {
+                req.flash('error', '文章发表失败了');
                 res.redirect('back');
-            }else{
-                req.flash('success','文章发表成功');
+            } else {
+                req.flash('success', '文章发表成功');
                 res.redirect('/');
             }
         });
@@ -64,26 +112,29 @@ router.post('/add',auth.mustLogin,function(req,res){
 
 });
 
-router.get('/detail/:articleId',function(req,res){
+router.get('/detail/:articleId', function (req, res) {
     var articleId = req.params.articleId;
+    Model('Article').update({_id:articleId},{$inc:{pv:1}}).then(function () {
+        return  Model('Article').findById(articleId).populate('comments.user')
+    })
     //find findById findOne等等都返回一个promise对象
-    Model('Article').findById(articleId).then(function(doc){
-        res.render('article/detail',{title:'文章详情',article:doc});
-    }).catch(function(err){
-        req.flash('error','查询文章详情失败');
+   .then(function (doc) {
+        res.render('article/detail', {title: '文章详情', article: doc});
+    }).catch(function (err) {
+        req.flash('error', '查询文章详情失败');
         req.redirect('back');
     });
 });
 //删除文章
-router.get('/delete/:articleId',function(req,res){
+router.get('/delete/:articleId', function (req, res) {
     //先得到要删除的文章ID
     var articleId = req.params.articleId;
     //删除指定的文章
-    Model('Article').remove({_id:articleId}).then(function(data){
-        req.flash('success','删除文章成功');
+    Model('Article').remove({_id: articleId}).then(function (data) {
+        req.flash('success', '删除文章成功');
         res.redirect('/');
-    },function(error){
-        req.flash('error','删除文章失败');
+    }, function (error) {
+        req.flash('error', '删除文章失败');
         res.redirect('back');
     });
 });
@@ -91,15 +142,27 @@ router.get('/delete/:articleId',function(req,res){
  * 1. 点击编辑链接跳到增加文章的页面，并且回显文章原来的内容
  * 2. 进行修改编辑，修改完成后保存文章
  */
-router.get('/update/:articleId',function(req,res){
+router.get('/update/:articleId', function (req, res) {
     //先得到要编辑的文章ID
     var articleId = req.params.articleId;
     //根据文章的ID查询文章的文档对象
-    Model('Article').findById(articleId).then(function(doc){
+    Model('Article').findById(articleId).then(function (doc) {
         //渲染模板，并传递当前的文章对象
-        res.render('article/add',{title:'编辑文章',article:doc});
-    }).catch(function (err) {
-        console.log(err)
+        res.render('article/add', {title: '编辑文章', article: doc});
+    })
+});
+router.post('/comment', function (req, res) {
+    var comment=req.body;//articleId content
+    Model('Article').update({_id:comment.articleId},
+        {
+        $push:{comments:{
+            content:comment.content,
+            user:req.session.user._id
+        }}
+    }).then(function (result) {
+        res.redirect('/article/detail/'+comment.articleId);
+    }, function () {
+        res.redirect('back');
     })
 });
 module.exports = router;
